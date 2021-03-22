35c827b3b74174e12f34c9e0de5255503ecc34df
Santy <santimarcelofierrod@gmail.com>
2021-03-22 13:00:47 -0500

    Implementacion base de datos tienda (Video 51)

--- Curso_Django/tienda_app/admin.py
+++ Curso_Django/tienda_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Producto, CategoriaProductos
+
 
 # Register your models here.
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    readonly_fields=("created", "updated")
+
+class CategoriaProductosAdmin(admin.ModelAdmin):
+    readonly_fields=("created", "updated")
+
+admin.site.register(CategoriaProductos, CategoriaProductosAdmin)
+admin.site.register(Producto, ProductoAdmin)
